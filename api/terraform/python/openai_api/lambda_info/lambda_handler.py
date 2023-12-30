dd6c0ac169fe89c1beb8bbbef0f4cd6bcf3c9bcb
lpm0073 <lpm0073@gmail.com>
2023-12-30 10:17:23 -0600

    chore: remove unused import

--- api/terraform/python/openai_api/lambda_info/lambda_handler.py
+++ api/terraform/python/openai_api/lambda_info/lambda_handler.py
@@ -2,8 +2,6 @@
 # pylint: disable=R0911,R0912,W0718
 """Lambda entry point for /info"""
 
-import json
-
 from openai_api.common.aws import aws_infrastructure_config as aws_config
 from openai_api.common.conf import settings
 from openai_api.common.utils import http_response_factory
